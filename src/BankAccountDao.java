61fe7cdb0a07381c22f8f55b710b061a50f0cc11
shukla <aayush.shukla@capgemini.com>
2019-04-09 19:26:56 +0530

    added files

---
+++ src/BankAccountDao.java
@@ -0,0 +1,20 @@
+package com.capgemini.bankaccount.dao;
+
+import java.util.List;
+
+import com.capgemini.bankapp.model.BankAccount;
+
+public interface BankAccountDao {
+	
+	public double getBalance(long accountId);
+	public boolean updateBalance(long accountId , double newBalance);
+	public boolean deleteBankAccouont(long accountId);
+	public boolean addNewAccount(BankAccount account);
+	public List<BankAccount> findAllBankAccountDetails();
+	public BankAccount findAccountById(long accountId);
+	public void rollback();
+	public void commit();
+	public boolean updateBankAccountDetails(long account_id , String customer_name , 	String account_type);
+
+	
+}
